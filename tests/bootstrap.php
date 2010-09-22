096359676959224bca7d2dd39e12fb4a33f99990
Fabian Becker <halfdan@xnorfz.de>
2010-09-22 02:27:15 +0200

    Started to add PHPUnit tests. Testing should be done using NetBeans PHPUnit testing suite (inclusive bootstrapping).

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author halfdan
+ */
+
+// TODO: check include path
+ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../../../../../../Program Files (x86)/EasyPHP-5.3.3/php/PEAR');
+
+// put your code here
+define('MONGOUI_ROOT', __DIR__.'/../');
+?>
